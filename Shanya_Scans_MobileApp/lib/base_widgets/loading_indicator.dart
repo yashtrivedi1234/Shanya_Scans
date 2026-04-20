1978b0c241eab825df49ba8d71b4dc37f17bf4b8
Yash Trivedi <yashtrivedi.contact@gmail.com>
2026-04-20 17:00:06 +0530

    done

--- Shanya_Scans_MobileApp/lib/base_widgets/loading_indicator.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:shanya_scans/ui_helper/app_colors.dart';
-
-Widget loadingIndicator ({Color? color}) {
-  return Center(child: SizedBox(width: 30,height: 30,  child: CircularProgressIndicator(color: color??AppColors.primary,)));
-}
